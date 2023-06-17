4ade97a46020621eb2ac5fd3e7c756f80852058e
lilbeb <orangeee04@gmail.com>
2023-06-17 18:19:04 +0300

    fix

--- ToDoList/ToDoList/Todoltem.swift
+++ ToDoList/ToDoList/Todoltem.swift
@@ -15,13 +15,13 @@ struct TodoItem {
         case important = "важная"
     }
    
-    var taskId: String
-    var text: String
-    var importance: Importance
-    var deadline: Date?
-    var isDone: Bool
-    var creationDate: Date
-    var modificationDate: Date?
+    let taskId: String
+    let text: String
+    let importance: Importance
+    let deadline: Date?
+    let isDone: Bool
+    let creationDate: Date
+    let modificationDate: Date?
     
     
     init(taskId: String = UUID().uuidString, text: String, importance: Importance, deadline: Date? = nil, isDone: Bool = false, creationDate: Date = Date(), modificationDate: Date? = nil) {
@@ -51,9 +51,9 @@ extension TodoItem{
             }
             var importance = Importance.usual
             if let dictImportance = dict["importance"] as? String,
-                let dictImportance = Importance(rawValue: dictImportance) {
-                    importance = dictImportance
-                }
+               let dictImportance = Importance(rawValue: dictImportance) {
+                importance = dictImportance
+            }
             let deadline = dict["deadline"]as? Date
             let modificationDate = dict["modificationDate"]as? Date
             
@@ -70,83 +70,71 @@ extension TodoItem{
     }
     var json: Any {
         var dictionary: [String: Any] = [
-            
-            "taskId" = taskId,
-            "text" = text,
-            "importance" = importance,
-            "isDone" = isDone,
-            "creationDate" = creationDate,
-            "deadline" = deadline,
-            "modificationDate" = modificationDate,
-            
+            "text": text,
+            "isDone": isDone,
+            "importance": importance.rawValue
         ]
-        
-        if let deadline = self.deadline {
-            dictionary["deadline"] = deadline.stringRepresentation
-        }
-        
         return dictionary
     }
-    
-     var todoItems: [TodoItem]
-     let fileName: String
-     let fileManager = FileManager.default
-    
-    init(fileName: String) {
-        self.fileName = fileName
-        self.todoItems = []
-        loadFromFile()
-    }
-    
-    var allTodoItems: [TodoItem] {
-        return todoItems
-    }
-    
-    func addTask(_ item: TodoItem) {
-        if let index = todoItems.firstIndex(where: ) {
-            todoItems[index] = item
-        } else {
-            todoItems.append(item)
-        }
-        saveToFile()
-    }
-    
-    func removeTodoItem(with id: String) {
-        todoItems.removeAll(where:)
-        saveToFile()
-    }
-    
-    private func getPath() -> URL? {
-        guard let documentDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
-        let folderURL = documentDirectory.appendingPathComponent("TodoItems", isDirectory: true)
-        
-        if !fileManager.fileExists(atPath: folderURL.path) {
-            do {
-                try fileManager.createDirectory(at: folderURL, withIntermediateDirectories: true, attributes: nil)
-            } catch {
-                print("Error creating folder for TodoItems: \(error.localizedDescription)")
-                return nil
-            }
-        }
-        
-        return folderURL.appendingPathComponent(fileName)
-    }
-    
-    private func saveToFile() {
-        guard let path = getPath() else { return }
-        do {
-            let jsonData = try JSONEncoder().encode(todoItems)
-            try jsonData.write(to: path)
-        } catch {
-            print("Error writing to file: \(error.localizedDescription)")
-        }
-    }
-    
-    private mutating func loadFromFile() {
-        guard let path = getPath(),
-              let jsonData = try? Data(contentsOf: path) else { return }
-        do {
-            todoItems = try JSONDecoder().decode([TodoItem].self, from: jsonData)
-        }
-    }
 }
+//     var todoItems: [TodoItem]
+//     let fileName: String
+//     let fileManager = FileManager.default
+//
+//    init(fileName: String) {
+//        self.fileName = fileName
+//        self.todoItems = []
+//        loadFromFile()
+//    }
+//
+//    var allTodoItems: [TodoItem] {
+//        return todoItems
+//    }
+//
+//    func addTask(_ item: TodoItem) {
+//        if let index = todoItems.firstIndex(where: ) {
+//            todoItems[index] = item
+//        } else {
+//            todoItems.append(item)
+//        }
+//        saveToFile()
+//    }
+//
+//    func removeTodoItem(with id: String) {
+//        todoItems.removeAll(where:)
+//        saveToFile()
+//    }
+//
+//    private func getPath() -> URL? {
+//        guard let documentDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+//        let folderURL = documentDirectory.appendingPathComponent("TodoItems", isDirectory: true)
+//
+//        if !fileManager.fileExists(atPath: folderURL.path) {
+//            do {
+//                try fileManager.createDirectory(at: folderURL, withIntermediateDirectories: true, attributes: nil)
+//            } catch {
+//                print("Error creating folder for TodoItems: \(error.localizedDescription)")
+//                return nil
+//            }
+//        }
+//
+//        return folderURL.appendingPathComponent(fileName)
+//    }
+//
+//  private func saveToFile() {
+//        guard let path = getPath() else { return }
+//        do {
+//            let jsonData = try JSONEncoder().encode(todoItems)
+//            try jsonData.write(to: path)
+//        } catch {
+//            print("Error writing to file: \(error.localizedDescription)")
+//        }
+//    }
+//
+//    private mutating func loadFromFile() {
+//        guard let path = getPath(),
+//              let jsonData = try? Data(contentsOf: path) else { return }
+//        do {
+//            todoItems = try JSONDecoder().decode([TodoItem].self, from: jsonData)
+//        }
+//    }
