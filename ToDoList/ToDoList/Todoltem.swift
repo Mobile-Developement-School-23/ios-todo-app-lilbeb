2ededced6e5ee2f750aa2de3c62ea8ab8c2ed336
lilbeb <orangeee04@gmail.com>
2023-06-18 14:00:19 +0300

    fix

--- ToDoList/ToDoList/Todoltem.swift
+++ ToDoList/ToDoList/Todoltem.swift
@@ -37,7 +37,7 @@ struct TodoItem {
 }
 
 
-extension TodoItem{
+extension TodoItem {
     
     static func parse(json: Any) -> TodoItem? {
         do {
@@ -77,64 +77,4 @@ extension TodoItem{
         return dictionary
     }
 }
-//     var todoItems: [TodoItem]
-//     let fileName: String
-//     let fileManager = FileManager.default
-//
-//    init(fileName: String) {
-//        self.fileName = fileName
-//        self.todoItems = []
-//        loadFromFile()
-//    }
-//
-//    var allTodoItems: [TodoItem] {
-//        return todoItems
-//    }
-//
-//    func addTask(_ item: TodoItem) {
-//        if let index = todoItems.firstIndex(where: ) {
-//            todoItems[index] = item
-//        } else {
-//            todoItems.append(item)
-//        }
-//        saveToFile()
-//    }
-//
-//    func removeTodoItem(with id: String) {
-//        todoItems.removeAll(where:)
-//        saveToFile()
-//    }
-//
-//    private func getPath() -> URL? {
-//        guard let documentDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
-//        let folderURL = documentDirectory.appendingPathComponent("TodoItems", isDirectory: true)
-//
-//        if !fileManager.fileExists(atPath: folderURL.path) {
-//            do {
-//                try fileManager.createDirectory(at: folderURL, withIntermediateDirectories: true, attributes: nil)
-//            } catch {
-//                print("Error creating folder for TodoItems: \(error.localizedDescription)")
-//                return nil
-//            }
-//        }
-//
-//        return folderURL.appendingPathComponent(fileName)
-//    }
-//
-//  private func saveToFile() {
-//        guard let path = getPath() else { return }
-//        do {
-//            let jsonData = try JSONEncoder().encode(todoItems)
-//            try jsonData.write(to: path)
-//        } catch {
-//            print("Error writing to file: \(error.localizedDescription)")
-//        }
-//    }
-//
-//    private mutating func loadFromFile() {
-//        guard let path = getPath(),
-//              let jsonData = try? Data(contentsOf: path) else { return }
-//        do {
-//            todoItems = try JSONDecoder().decode([TodoItem].self, from: jsonData)
-//        }
-//    }
+
